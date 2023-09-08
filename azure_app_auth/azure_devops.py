09c75195a82e41645239abd3a44c802ab82564c8
DJ <heyymrdj@protonmail.com>
2023-09-08 15:26:28 -0400

    Major refactor

--- azure_app_auth/azure_devops.py
+++ azure_app_auth/azure_devops.py
@@ -40,12 +40,7 @@ def auth(
         token_info = token_response.json()
         return token_info["access_token"]
 
-    print(
-        f"""Failed to acquire a token.
-          Status code: {token_response.status_code}"""
-    )
-    print(token_response.text)
-    return None
+    return "Failed to create access Token"
 
 
 def create_pat(
@@ -57,7 +52,7 @@ def create_pat(
 
     # Define the token request payload
     api_data = json.dumps(
-        {"displayName": f"{token_name}", "scope": "app_token"}
+        {"displayName": f"{token_name}", "scope": "vso.code"}
     )  # noqa: E501 # pylint: disable=line-too-long
 
     api_headers = {
@@ -75,6 +70,113 @@ def create_pat(
 
     print(response.text)
     return f"Access code {token_name} failed to create"
+
+
+def update_pat(
+    access_token: str, organization_name: str, token_id: str, token_name: str
+) -> str:  # noqa: E501
+    """Function to update PAT"""
+    # NOTE: AuthorizationID must be passed as data instead of URI
+
+    api_url = f"https://vssps.dev.azure.com/{organization_name}/_apis/tokens/pats?&api-version=7.0-preview.1"  # noqa: E501  # pylint: disable=line-too-long
+
+    # Define the token request payload
+    api_data = json.dumps(
+        {"authorizationId": f"{token_id}", "displayName": f"{token_name}"}
+    )  # noqa: E501 # pylint: disable=line-too-long
+
+    api_headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    # Send POST response to create PAT
+    response = requests.request(
+        "PUT", api_url, headers=api_headers, data=api_data, timeout=10
+    )  # noqa: E501
+    if response.status_code == 200:
+        return response.text
+
+    print(response.text)
+    return "ERROR Revoking PAT"
+
+
+def revoke_pat(
+    access_token: str, organization_name: str, token_id: str
+) -> str:  # noqa: E501
+    """Function to revoke PAT"""
+
+    api_url = f"https://vssps.dev.azure.com/{organization_name}/_apis/tokens/pats?authorizationId={token_id}&api-version=7.0-preview.1"  # noqa: E501  # pylint: disable=line-too-long
+
+    # Define the token request payload
+    api_data = json.dumps({})  # noqa: E501 # pylint: disable=line-too-long
+
+    api_headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    # Send POST response to create PAT
+    response = requests.request(
+        "DELETE", api_url, headers=api_headers, data=api_data, timeout=10
+    )  # noqa: E501
+    if response.status_code == 204:
+        return response.text
+
+    print(response.text)
+    return "ERROR Revoking PAT"
+
+
+def get_pat(
+    access_token: str, organization_name: str, token_id: str
+) -> str:  # noqa: E501
+    """Function to get info for single PAT"""
+
+    api_url = f"https://vssps.dev.azure.com/{organization_name}/_apis/tokens/pats?authorizationId={token_id}&api-version=7.0-preview.1"  # noqa: E501  # pylint: disable=line-too-long
+
+    # Define the token request payload
+    api_data = json.dumps({})  # noqa: E501 # pylint: disable=line-too-long
+
+    api_headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    # Send POST response to create PAT
+    response = requests.request(
+        "GET", api_url, headers=api_headers, data=api_data, timeout=10
+    )  # noqa: E501
+    if response.status_code == 200:
+        pat_info = response.json()
+        return pat_info
+
+    print(response.text)
+    return "ERROR getting PAT"
+
+
+def list_pats(access_token: str, organization_name: str) -> str:  # noqa: E501
+    """Function to list all PATs"""
+
+    api_url = f"https://vssps.dev.azure.com/{organization_name}/_apis/tokens/pats?api-version=7.0-preview.1"  # noqa: E501  # pylint: disable=line-too-long
+
+    # Define the token request payload
+    api_data = json.dumps({})  # noqa: E501 # pylint: disable=line-too-long
+
+    api_headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    # Send POST response to create PAT
+    response = requests.request(
+        "GET", api_url, headers=api_headers, data=api_data, timeout=10
+    )  # noqa: E501
+    if response.status_code == 200:
+        pat_info = response.json()
+        return pat_info
+
+    print(response.text)
+    return "ERROR LISTING PATs"
 
 
 def test():
--- pat_rotate.py
+++ pat_rotate.py
@@ -1,24 +1,93 @@
 """Python script to rotate ADO PATs using custom libraries"""
-import sys
+import argparse
 from azure_app_auth import azure_devops
 
 
-if len(sys.argv[:]) < 8:
-    print("Missing required arguments")
-    sys.exit()
-else:
-    TENANT_ID = sys.argv[1]
-    CLIENT_ID = sys.argv[2]
-    CLIENT_SECRET = sys.argv[3]
-    USERNAME = sys.argv[4]
-    PASSWORD = sys.argv[5]
-    ORGANZATION_NAME = sys.argv[6]
-    TOKEN_NAME = sys.argv[7]
+def parse_arguments():
+    """Function to parse CLI arguments"""
 
-access_token = azure_devops.auth(
-    TENANT_ID, CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD
-)
+    parser = argparse.ArgumentParser(
+        description="Azure DevOps Authentication and Token Management"
+    )  # noqa: E501  # pylint: disable=line-too-long
+    parser.add_argument(
+        "operation",
+        choices=["create", "get", "revoke", "list", "update"],
+        help="Operation to perform",
+    )  # noqa: E501  # pylint: disable=line-too-long
+    parser.add_argument(
+        "--tenant_id", required=True, help="Azure AD Tenant ID"
+    )  # noqa: E501  # pylint: disable=line-too-long
+    parser.add_argument(
+        "--client_id", required=True, help="Azure AD Client ID"
+    )  # noqa: E501  # pylint: disable=line-too-long
+    parser.add_argument(
+        "--client_secret", required=True, help="Azure AD Client Secret"
+    )  # noqa: E501  # pylint: disable=line-too-long
+    parser.add_argument(
+        "--username", required=True, help="Azure DevOps Username"
+    )  # noqa: E501  # pylint: disable=line-too-long
+    parser.add_argument(
+        "--password", required=True, help="Azure DevOps Password"
+    )  # noqa: E501  # pylint: disable=line-too-long
+    parser.add_argument(
+        "--organization_name",
+        required=True,
+        help="Azure DevOps Organization Name",  # noqa: E501  # pylint: disable=line-too-long
+    )
+    parser.add_argument("--token_name", help="Token Name for PAT operations")
+    parser.add_argument("--token_id", help="Token ID for PAT operations")
+    args = parser.parse_args()
 
-result = azure_devops.create_pat(access_token, ORGANZATION_NAME, TOKEN_NAME)
+    if args.operation in ["create", "update"] and args.token_name is None:
+        parser.error(
+            f"--token_name is required for the {args.operation} operation"
+        )  # noqa: E501  # pylint: disable=line-too-long
 
-print(result)
+    if args.operation in ["revoke", "get", "update"] and args.token_id is None:
+        parser.error(
+            f"--token_id is required for the {args.operation} operation"
+        )  # noqa: E501  # pylint: disable=line-too-long
+
+    return parser.parse_args()
+
+
+def main():
+    """Main Function"""
+    args = parse_arguments()
+
+    access_token = azure_devops.auth(
+        args.tenant_id,
+        args.client_id,
+        args.client_secret,
+        args.username,
+        args.password,  # noqa: E501  # pylint: disable=line-too-long
+    )
+
+    if args.operation == "create":
+        result = azure_devops.create_pat(
+            access_token, args.organization_name, args.token_name
+        )  # noqa: E501  # pylint: disable=line-too-long
+
+    elif args.operation == "get":
+        result = azure_devops.get_pat(
+            access_token, args.organization_name, args.token_id
+        )  # noqa: E501  # pylint: disable=line-too-long
+
+    elif args.operation == "revoke":
+        result = azure_devops.revoke_pat(
+            access_token, args.organization_name, args.token_id
+        )  # noqa: E501  # pylint: disable=line-too-long
+
+    elif args.operation == "list":
+        result = azure_devops.list_pats(access_token, args.organization_name)
+
+    elif args.operation == "update":
+        result = azure_devops.update_pat(
+            access_token, args.organization_name, args.token_id, args.token_name
+        )  # noqa: E501  # pylint: disable=line-too-long
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
